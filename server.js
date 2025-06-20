6e33a6cba32356517c99a45183062b0f8a2d9a5c
Patrick Moore <patrickwmoore88@gmail.com>
2025-06-19 23:27:24 -0500

    adjusting app.listen for railway

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const app = require('./clientTell');
 
-const port = process.env.PORT
+// const port = process.env.PORT;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server up and running.`);
