a5fc22a67591a39ccb9dd7892f5dd9b6f96af96b
Patrick Moore <patrickwmoore88@gmail.com>
2025-06-19 23:12:05 -0500

    making minor adjustments

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 const app = require('./clientTell');
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port: ${process.env.PORT}`);
+  console.log(`Server up and running.`);
 });
